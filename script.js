fa3ffaab30e895750f98446fa11655cda406d169
Lucas-9017 <111684@emmauscollege.nl>
2020-09-09 10:20:37 +0000

    Bewegend balletje

--- script.js
+++ script.js
@@ -6,9 +6,6 @@
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
-
-  // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
 }
 
 
@@ -18,9 +15,30 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+    background("blue");
+
   // stel vulkleur in
-  fill(100, 100, 255);
+  fill(0, 255, 0);
+
+  var xBal = 100;
+  var yBal = 100;
+  var radius = 100;
+  var xSpeed = 10;
+  var ySpeed = 10;
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(xBal, yBal, radius, radius);
+
+  xBal = xBal + xSpeed;
+  yBal = yBal + ySpeed;
+
+  if (xBal >= 1280 || xBal <= 0) {
+      xSpeed = xSpeed * -1;
+  }
+
+  if (yBal >= 720 || yBal <= 0) {
+      ySpeed = ySpeed * -1;
+  }
+
+
 }
